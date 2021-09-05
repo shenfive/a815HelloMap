742d247094a8e672d955b8202d9dc00ae0f78e78
Danny Shen <shenfive@hotmail.com>
2021-09-05 16:18:28 +0800

    m1

--- a815HelloMap/ViewController.swift
+++ a815HelloMap/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let latitude:CLLocationDegrees = 25.0444032
+            let longitude:CLLocationDegrees = 121.5141468
+            let location:CLLocationCoordinate2D =
+                CLLocationCoordinate2DMake(latitude, longitude)
+            let xScale:CLLocationDegrees = 0.001
+            let yScale:CLLocationDegrees = 0.001
+            let span:MKCoordinateSpan =
+                MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+            let region:MKCoordinateRegion =
+                MKCoordinateRegion.init(center: location, span: span)
+            self.theMapView.setRegion(region, animated: true)
+        }
+    }
+    
+    
 }
 
