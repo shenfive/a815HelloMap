5f179cf575a789d12de3b833719031a6d41169b2
Danny Shen <shenfive@hotmail.com>
2021-09-05 16:35:36 +0800

    no scr

--- a815HelloMap/ViewController.swift
+++ a815HelloMap/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var theMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        theMapView.isScrollEnabled = false
         // Do any additional setup after loading the view.
     }
 
