869502b799f554f26a85f5c7c74bf3fd73b88a25
Danny Shen <shenfive@hotmail.com>
2021-09-05 16:31:51 +0800

    map type

--- a815HelloMap/ViewController.swift
+++ a815HelloMap/ViewController.swift
@@ -20,12 +20,12 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            let latitude:CLLocationDegrees = 25.0444032
-            let longitude:CLLocationDegrees = 121.5141468
+            let latitude:CLLocationDegrees = 25.0443651
+            let longitude:CLLocationDegrees = 121.5132172
             let location:CLLocationCoordinate2D =
                 CLLocationCoordinate2DMake(latitude, longitude)
-            let xScale:CLLocationDegrees = 0.001
-            let yScale:CLLocationDegrees = 0.001
+            let xScale:CLLocationDegrees = 0.0001
+            let yScale:CLLocationDegrees = 0.0001
             let span:MKCoordinateSpan =
                 MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
             let region:MKCoordinateRegion =
@@ -33,6 +33,25 @@ class ViewController: UIViewController {
             self.theMapView.setRegion(region, animated: true)
         }
     }
+    
+    
+    @IBAction func setMapType(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            theMapView.mapType = .standard
+        case 1:
+            theMapView.mapType = .satellite
+        case 2:
+            theMapView.mapType = .hybrid
+        default:
+            break
+        }
+        
+        
+        
+        
+    }
+    
     
     
 }
